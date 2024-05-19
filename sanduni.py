ee75bdae6dffbcda9a8133cb840390633f77e082
heshanPasindu <heshanpasindugunawardhana@gmail.com>
2024-05-19 23:06:41 +0530

    Heshan

--- sanduni.py
+++ sanduni.py
@@ -11,6 +11,17 @@ print(sri_lanka_data.head())
 # Filter the DataFrame to include only records related to Sri Lanka
 sri_lanka_data = data[data['Entity'] == 'Sri Lanka
 
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Model training
+model = LinearRegression()
+model.fit(X_train, y_train)
+
 # Predictions
 y_pred_train = model.predict(X_train)
 y_pred_test = model.predict(X_test)
+
+# Model evaluation
+train_rmse = np.sqrt(mean_squared_error(y_train, y_pred_train))
+test_rmse = np.sqrt(mean_squared_error(y_test, y_pred_test))
